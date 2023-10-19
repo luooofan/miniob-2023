dbdb093285ab9530f8d0ef6337943d9130557508
luooofan <luooofan@qq.com>
2023-10-20 01:28:41 +0800

    support typecast. change to float

--- src/observer/sql/parser/value.cpp
+++ src/observer/sql/parser/value.cpp
@@ -227,6 +227,10 @@ int Value::compare(const Value &other) const
   } else if (this->attr_type_ == FLOATS && other.attr_type_ == INTS) {
     float other_data = other.num_value_.int_value_;
     return common::compare_float((void *)&this->num_value_.float_value_, (void *)&other_data);
+  } else {
+    float this_data = this->get_float();
+    float other_data = other.get_float();
+    return common::compare_float((void *)&this_data, (void *)&other_data);
   }
   LOG_WARN("not supported");
   return -1;  // TODO return rc?
