fd656b40701d8c3d0c4d00f03f75ac6b106212f6
luooofan <luooofan@qq.com>
2023-10-24 22:16:11 +0800

    update build.sh. no need to gen_parser before test cases

--- build.sh
+++ build.sh
@@ -241,6 +241,8 @@ function main
       do_clean
       ;;
     test)
+      cd ./src/observer/sql/parser && bash gen_parser.sh && cd ../../../../
+      echo "gen parser finish"
       do_test "$@"
       ;;
     diff)
